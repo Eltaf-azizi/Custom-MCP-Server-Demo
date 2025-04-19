3cc39ecb5070df83cffb43482d09c8531abe9b43
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-19 16:29:57 +0500

    working on the read_notes function

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 # Create an MCP server
 mcp = FastMCP("AI Sticky Notes")
 
-NOTES_FILE = "notes.txt"
+NOTES_FILE = os.path.join(os.path.dirname(__file__), "NOTES.txt")
 
 
 def ensure_file():
@@ -16,7 +16,33 @@ def ensure_file():
 
 @mcp.tool()
 def add_note(message: str) -> str:
+    """
+    Append a new note to the sticky note file.
+
+    Args:
+        message (str): The note content to be added.
+
+    Returns:
+        str: Confirmation message indicating the note was saved.
+    """
     ensure_file()
     with open(NOTES_FILE, "a") as f:
         f.write(message + "\n")
     return "Note Saved!"
+
+
+
+@mcp.tool()
+def read_notes() -> str:
+    """
+    Read the return all notes from the sticky note file.
+
+    Returns:
+        str: All notes as a single string seperated by line breaks.
+            If no notes exist, a default message is returned.
+    """
+    ensure_file()
+    with open(NOTES_FILE, "r") as f:
+        content = f.read().strip()
+
+    return content or "No notes yet."
